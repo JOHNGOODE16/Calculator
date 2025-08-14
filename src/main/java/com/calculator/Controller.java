72847c739227598ada8c57b56c5821074a2e7a4a
Johnathan Goode <89587677+JOHNGOODE16@users.noreply.github.com>
2025-08-14 10:27:39 -0400

    ADD onAddition() method to controller/Label to display history

--- src/main/java/com/calculator/Controller.java
+++ src/main/java/com/calculator/Controller.java
@@ -3,10 +3,12 @@ package com.calculator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class Controller {
 	@FXML private TextField display;
+	@FXML private Label history;
 	
 	@FXML
 	private void onDigit(ActionEvent e) {
@@ -22,6 +24,15 @@ public class Controller {
 	@FXML
 	private void onClear(ActionEvent e) {
 		display.clear();
+	}
+	
+	@FXML
+	private void onAddition(ActionEvent e) {
+		String d =((Button) e.getSource()).getText();
+		if(!(display.getText().isEmpty() && display.getText() == null)) {
+			history.setText(display.getText() + " " + d);
+		}
+		
 	}
 
 }
